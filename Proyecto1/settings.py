92489ff5f42ddc16049742e166dfc2057a9d1f46
Daniel <munozd70@gmail.com>
2020-10-16 22:42:34 -0500

    itachi, 13 commit

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -138,14 +138,14 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/ITACHIGRIAS/Proyecto1/static/'
+# STATIC_ROOT = '/home/ITACHIGRIAS/Proyecto1/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "/static/"),)
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
 # )
 #
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #
