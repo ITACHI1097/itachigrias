53f826bdf2921311d6285d58b3c89ca14cac7954
Daniel <munozd70@gmail.com>
2020-10-16 23:38:30 -0500

    itachi, static1 commit

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2%ef25k)lv$yfpvo8&)y3)e06njb@cf+p^_obp#fl#o+mpd1xj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -141,13 +141,13 @@ USE_TZ = True
 # STATIC_ROOT = '/home/ITACHIGRIAS/Proyecto1/static/'
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "/static/"),)
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 #
-# STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #
 # STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 #
