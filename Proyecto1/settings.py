12f699b90734416bd9a7704cab2eccb3fd03c0c6
Daniel <munozd70@gmail.com>
2020-10-16 19:35:15 -0500

    itachi, tercer commit

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Application definition
 
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Proyecto1.urls'
