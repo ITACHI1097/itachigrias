d112ded992e604750dadbe13412420ada0254b32
Daniel <munozd70@gmail.com>
2020-10-16 22:01:57 -0500

    itachi, 8 commit

--- Proyecto1/settings.py
+++ Proyecto1/settings.py
@@ -144,9 +144,9 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #
-# #STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
 #
 # MEDIA_URL = "/media/"
 #
