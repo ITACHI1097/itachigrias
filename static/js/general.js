5483a52a038d7f4c6af480fffe0877faa3d7e11f
Daniel <munozd70@gmail.com>
2020-12-17 16:25:01 -0500

    tesis corregido algunos errorres

--- static/js/general.js
+++ static/js/general.js
@@ -455,7 +455,7 @@ $(document).ready(function (){
                                                         <div class="card shadow mb-4">
                                                                 <div class="card-body">
                                                                     <div class="table-responsive">
-                                                                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                                                        <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0">
                                                                             <thead>
                                                                                 ${tableHeader}
                                                                             </thead>
@@ -968,7 +968,14 @@ $(document).ready(function (){
                                                             <div class="card shadow mb-4">
                                                                     <div class="card-body">
                                                                         <div class="table-responsive">
-                                                                            <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0"><thead>${tableHeader}</thead><tbody>${tableBody}</tbody></table>
+                                                                            <table class="table table-striped table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                                                                <thead>
+                                                                                    ${tableHeader}
+                                                                                </thead>
+                                                                                <tbody>
+                                                                                    ${tableBody}
+                                                                                </tbody>
+                                                                            </table>
                                                                         </div>
                                                                     </div>
                                                                 </div>
