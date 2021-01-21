9c1f0898164fcaf1bcc0062e0b6063acdf490e3f
Daniel <munozd70@gmail.com>
2021-01-20 23:25:00 -0500

    corregido error de correo electronico

--- Proyecto1/urls.py
+++ Proyecto1/urls.py
@@ -52,7 +52,7 @@ urlpatterns = [
     path('general-dashboard/', viewsGeneral.Dashboard, name='Dashboard'),
     path('genero-anio/', viewsCubos.generoAno, name='genero-anio'),
     # path('grafic/Dashboard/prueba.html', views.prueba)
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='Dashboard/password_reset.html'), name='reset_password'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='Dashboard/password_reset.html', email_template_name='Dashboard/password_reset_email.html'), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='Dashboard/password_reset_sent.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='Dashboard/password_reset_form.html'), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='Dashboard/password_reset_done.html'), name='password_reset_complete'),
